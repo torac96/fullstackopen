c0712a5e26d2c45f5e83d37edbf1d9ae91c743ab
LAPTOP-Q153GUHP\pietr <peter.96.torac@gmail.com>
2022-01-21 08:38:19 +0100

    completed exercise 1.13 anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react'
 
-const Button = ({handleClick, maxLength}) => {
-   const createNumber = () => Math.floor(Math.random() * maxLength);
-
-  return (
-    <button onClick={() => handleClick(createNumber())}>Next anecdote </button>
-  )
-}
+const Button = ({handleClick, text}) => <button onClick={handleClick}> {text} </button>
 
 const App = () => {
   const anecdotes = [
@@ -20,13 +14,29 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleNextClick = () => {
+    const number = () => Math.floor(Math.random() * anecdotes.length);
+    setSelected(number)
+  }
+
+  const handleVoteClick = () => {
+    const copy = [ ...points ]
+    copy[selected] = copy[selected] + 1  
+    setPoints(copy)
+  }
 
   return (
     <>
       <div>
         {anecdotes[selected]}
       </div>
-      <Button handleClick={(number) => setSelected(number)} maxLength={anecdotes.length}/>
+      <div>
+        has {points[selected]} votes
+      </div>
+      <Button handleClick={() => handleVoteClick()} text="vote"/>
+      <Button handleClick={() => handleNextClick()} text="Next anecdote"/>
     </>
   )
 }
