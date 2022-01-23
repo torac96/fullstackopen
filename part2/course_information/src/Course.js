a7145b3fdcef0dbf3edef972d2b5bd2cadb1d70a
LAPTOP-Q153GUHP\pietr <peter.96.torac@gmail.com>
2022-01-23 14:18:39 +0100

    completed exercise 2.4 Course information

--- part2/course_information/src/Course.js
+++ part2/course_information/src/Course.js
@@ -1,39 +1,42 @@
 import React from 'react';
 
-const Header = ({course}) => <h1>{course}</h1>
+const Header = ({ course }) => <h1>{course}</h1>
 
 const Part = (props) => <p>{props.content}</p>
 
-const Content = ({parts}) => (
+const Content = ({ parts }) => (
   <div>
     {
-      parts && parts.map( elem => 
-        (
-          elem.name && <Part key={elem.name} content={elem.name + ' ' + (elem.exercises ? elem.exercises : '')} /> 
-        )
+      parts && parts.map(elem =>
+      (
+        elem.name && <Part key={elem.name} content={elem.name + ' ' + (elem.exercises ? elem.exercises : '')} />
+      )
       )
     }
   </div>
 )
 
-const Total = ({parts}) => {
-  const total = parts.reduce((sum, value) =>{
-    return (value.name && value.exercises>0 ) ? sum +value.exercises : sum
+const Total = ({ parts }) => {
+  const total = parts && parts.reduce((sum, value) => {
+    return (value.name && value.exercises > 0) ? sum + value.exercises : sum;
   }, 0)
 
   return (
-    <b>total of {total} exercises</b>
+    total >0 ? 
+      <b>total of {total} exercises</b>
+      :
+      <b>there is no exercises yet</b>
   );
 }
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
   return (
-		<div>
-			<Header course={course.name} />
+    <div>
+      <Header course={course.name} />
       <Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</div>
-	)
+      <Total parts={course.parts} />
+    </div>
+  )
 }
 
 export default Course;
